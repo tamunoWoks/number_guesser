86cfaf6eb38a60ab8d775284137c72b665582661
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-11 00:34:42 +0100

    Check if input is greater than 0

--- guesser.py
+++ guesser.py
@@ -4,3 +4,7 @@ range_limit = input("Type a number: ").strip()
 
 if range_limit.isdigit():
     range_limit = int(range_limit)
+
+    if range_limit <= 0:
+        print('You must enter a number larger than 0')
+        quit()
