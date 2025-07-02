ace649aa144e16d1525ba7e915a433e7cc9a6bca
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-07-02 16:07:29 -0600

    Update guesser2.py

--- guesser2.py
+++ guesser2.py
@@ -1,1 +1,8 @@
-
+"""
+Guess the Number Game
+---------------------
+The computer selects a random number between 1-20,
+and the player has 6 attempts to guess it.
+After each guess, the computer provides feedback
+(too high/too low) until the player guesses correctly.
+"""
